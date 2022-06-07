6c6860f46f5afb7551c743963290f082dd779c15
griselizr <griseliz.r.c15@gmail.com>
2022-06-07 17:52:59 +0000

    first commit

---
+++ hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - return type
+ *Return: 0
+ */
+int main ()
+{
+	char charType;
+	int intType:
+	float floatType;
+	long int  longintType;
+	long long int longlongintType;
+	float floatType;
+
+	printf("size of a char: %c bytes\n",sizeof(charType));
+	printf("size of a int: %i bytes\n",sizeof(intType));
+	printf("size of a long int: %ld bytes\n",sizeof(fType));
+	printf("size of a long long int: %ld bytes\n",sizeof(longlongintType));
+	printf("size of a float: %f bytes\n",sizeof(floatType));
+	
+	return (0);
+}
+
