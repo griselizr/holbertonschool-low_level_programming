bc23733b094c077e7783226176edff07f0d2e1fe
griselizr <griseliz.r.c15@gmail.com>
2022-06-07 18:01:53 +0000

    first commit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main ()
 {
-	printf("size of a char: %ld bytes\n",sizeof(char));
-	printf("size of a int: %ld bytes\n",sizeof(int));
-	printf("size of a long int: %ld bytes\n",sizeof(long int));
-	printf("size of a long long int: %ld bytes\n",sizeof(long long int));
-	printf("size of a float: %ld bytes\n",sizeof(float));
+	printf("size of a char: %ld bytes(s)\n",sizeof(char));
+	printf("size of an int: %ld bytes(s)\n",sizeof(int));
+	printf("size of a long int: %ld bytes(s)\n",sizeof(long int));
+	printf("size of a long long int: %ld bytes(s)\n",sizeof(long long int));
+	printf("size of a float: %ld bytes(s)\n",sizeof(float));
 	
 	return (0);
 }
