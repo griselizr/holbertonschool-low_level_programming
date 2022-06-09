441ddcddaf57eeda912aead02e9607d199091a25
griselizr <griseliz.r.c15@gmail.com>
2022-06-09 15:07:33 +0000

    first commit

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ *_putchar- entry point
+ *
+ */
+int _putchar(char c)
+{
+	char w;
+
+	w = '_putchar';
+	
+	w = 0;
+	while (w <= 10)
+		_putchar(w);
+		_putchar('\n');
+	return 0;
+}
