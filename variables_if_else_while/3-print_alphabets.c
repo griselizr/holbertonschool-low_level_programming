fb3c48ef9c0a651277ec2d1e3b0f9d1d3631cef5
griselizr <griseliz.r.c15@gmail.com>
2022-06-09 03:37:22 +0000

    first commit

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Return: 0
+ */
+int main(void)
+{
+	int  low;
+	int up;
+
+	/* lower case letters */
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	/* upper case letters */
+	for (up = 'A'; up <= 'Z'; up++)
+	{
+		putchar(up);
+	}
+		putchar('\n');
+	    return (0);
+}
