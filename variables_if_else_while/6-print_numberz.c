9208459235d29b70bcc1976c2650097a9b337ab6
griselizr <griseliz.r.c15@gmail.com>
2022-06-09 05:11:28 +0000

    first commit

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main- entry point
+ *Return: 0
+ */
+int main(void)
+{
+	int num;
+	/* print numbers from 0 t0 10 with ASCII table*/
+	for (num = 48; num <= 57; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
+
+
