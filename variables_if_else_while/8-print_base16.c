e2ab05752148963d4ab15ee45fd5f1fe63548390
griselizr <griseliz.r.c15@gmail.com>
2022-06-09 05:03:41 +0000

    first commit

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Return: 0
+ */
+int main(void)
+{
+	int num;
+	char letters;
+
+	/* numbers from 0 to 9 */
+	for (num= 48; num <= 57; num++)
+		putchar(num);
+	/* hexadecimal letter a to f */
+	for (letters = 'a'; letters <= 'f'; letters++)
+	{
+		putchar(letters);
+	}
+		putchar('\n');
+return (0);
+}
