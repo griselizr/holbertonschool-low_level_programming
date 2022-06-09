00714476ce244a3fa2e6929d3276de079eeb686a
griselizr <griseliz.r.c15@gmail.com>
2022-06-09 05:06:03 +0000

    fix betty

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 	char letters;
 
 	/* numbers from 0 to 9 */
-	for (num= 48; num <= 57; num++)
+	for (num = 48; num <= 57; num++)
 		putchar(num);
 	/* hexadecimal letter a to f */
 	for (letters = 'a'; letters <= 'f'; letters++)
