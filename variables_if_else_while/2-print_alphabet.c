451fe1c2c58c80b14a73d5d638e75464a33450fd
griselizr <griseliz.r.c15@gmail.com>
2022-06-09 03:20:46 +0000

    first commit

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *
+ *main- entry point
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int idx;
+
+	for (idx = 'a'; idx <= 'z';idx++)
+	{
+		putchar(idx);
+	}
+	putchar('\n' );
+	return (0);
+}
