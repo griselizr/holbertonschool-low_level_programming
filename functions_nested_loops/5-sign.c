9b4df1fec286af48df963225dbaa6bb046b2bc11
griselizr <griseliz.r.c15@gmail.com>
2022-06-10 03:37:01 +0000

    first commit

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - checks for a alphabetic order
+ *@n: character to be checked
+ * Return: 1 if n is greater than cero, 0 if returns o, and -1 if its less than cero.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
