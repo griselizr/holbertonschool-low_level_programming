d75314d779f9f86b0a3f9f07de401ff3ef5cf604
griselizr <griseliz.r.c15@gmail.com>
2022-06-10 14:44:17 +0000

    first commit

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int_abs - absolute variable of an integer
+ * @num - parameter
+ * Return: 0 or 1
+ */
+
+int _abs(int num)
+{
+	if (num > 0)
+		return (num);
+	else
+		return (-num);
+}
