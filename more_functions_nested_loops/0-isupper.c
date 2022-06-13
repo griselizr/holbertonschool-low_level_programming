f474b74251a29961697e3da3d506ce229e953cf4
griselizr <griseliz.r.c15@gmail.com>
2022-06-13 14:55:48 +0000

    first commit

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: checks letters
+ * Return: 1 if uppercase, else 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
