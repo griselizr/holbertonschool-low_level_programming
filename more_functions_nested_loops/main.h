31cf9f651abbef2bb2f969c857669388247587c1
griselizr <griseliz.r.c15@gmail.com>
2022-06-13 13:39:08 +0000

    main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -5,6 +5,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+int main(void);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
