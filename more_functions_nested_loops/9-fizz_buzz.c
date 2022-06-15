698edd0e80b9fecfe4656b714f8b64c392a0f85f
griselizr <griseliz.r.c15@gmail.com>
2022-06-15 02:46:21 +0000

    fix code

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,14 +9,14 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 15) == 0)
+		if ((i % 3) == 0 && (i % 5) == 0)
 			printf("FizzBuzz ");
 		else if ((i % 3) == 0)
 			printf("Fizz ");
 		else if ((i % 5) == 0)
 			printf("Buzz ");
 		else
-			printf("%d", i);
+			printf("%d ", i);
 	}
 		printf("\n");
 		return (0);
