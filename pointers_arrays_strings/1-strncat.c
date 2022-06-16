05ceb0dedd88fa84ffb01d59b5277fe1bb0e1805
griselizr <griseliz.r.c15@gmail.com>
2022-06-16 21:25:13 +0000

    fix code

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,13 +10,14 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int a = 0, b;
 
-		while (dest[a] != '\0')
-			a++;
-		
-	for (b = 0; b < n; b++, a++)
-		dest[a] = src[b];
-		
+	while (dest[a] != '\0')
+	a++;
 
+	for (b = 0; src[b] != '\0' && b < n; b++)
+	{
+		dest[a + b] = src[b];
+	}
+		dest[a + b] = '\0';
 	return (dest);
 }
 
