4595ff4afcf2726b53b6c677e32327117f28c8fb
griselizr <griseliz.r.c15@gmail.com>
2022-06-17 02:19:12 +0000

    update

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,17 +7,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int a;
-
-	a = 0;
-	if (s1[a] == s2[a] && s1[a] != '\0' && s2[a] != '\0')
-		return (0);
-
-	if (s1[a] < s2[a])
-		a++;
-	if (s2[a] > s1[a])
-		return (s1[a] - s2[a]);
-	else
-
-		return (-(s2[a] - s1[a]));
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return (0);
+		s1++;
+		s2++;
+	}
+		return (*s1 - *s2);
 }
