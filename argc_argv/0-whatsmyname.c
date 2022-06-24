29c1386f50ecb7c5e00216abcaff342ed82529ab
griselizr <griseliz.r.c15@gmail.com>
2022-06-24 04:09:57 +0000

    first commit

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints program name
+ * @argc: count
+ * @argv: vector
+ * Return: 0
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	{
+		printf("argc = %s\n", argv[0]);
+	}
+	return (0);
+}
+
+
