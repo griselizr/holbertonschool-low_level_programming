261b873f9d9ae0d26b3dcac7f485b447663d9f13
griselizr <griseliz.r.c15@gmail.com>
2022-06-24 21:52:59 +0000

    first comit

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints program name
+ * @argc: count
+ * @argv: vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i= 0;
+	int mul = 0;
+	int total;
+
+	if (argc > 2)
+	{
+		i = atoi(argv[1]);
+		mul = atoi(argv[2]);
+		total = i * mul;
+
+		printf("%d\n", total);
+	}
+	else if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
