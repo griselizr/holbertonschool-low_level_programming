7481d303d48633d45000e779859fa3fb211d3d25
griselizr <griseliz.r.c15@gmail.com>
2022-06-29 02:22:56 +0000

    first commit

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenate two strings
+ *@s1: string
+ *@s2: string
+ *@n: bytes
+ * Return: Always 0.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *t;
+	unsigned int lk1, lk2, sum;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (lk1 = 0; s1[lk1]; lk1++)
+	;
+	for (lk2 = 0; s2[lk2]; lk2++)
+		;
+	if (lk2 > n)
+		lk2 = n;
+	t = malloc((sizeof(char) * (lk1 + lk2) + 1));
+		if (t == NULL)
+		return (NULL);
+
+	for (sum = 0; sum < lk1; sum++)
+	{
+		t[sum] = s1[sum];
+	}
+	for (sum = 0; sum  < lk2; sum++)
+	{
+		t[sum + lk1] = s2[sum];
+	}
+		t[lk1 + lk2] = '\0';
+	return (t);
+}
+
+
+
+
