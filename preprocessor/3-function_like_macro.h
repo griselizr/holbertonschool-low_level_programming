760e55e525c5abef4cbda8e9998e5ec9342b5e4b
griselizr <griseliz.r.c15@gmail.com>
2022-06-29 04:48:55 +0000

    first commit

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
-#define ABS(x) (((x) < 0) ? (x * -1) : (x))
+#ifndef _3_FUNCTION_LIKE_MACRO
+#define _3_FUNCTION_LIKE_MACRO
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
 #endif
