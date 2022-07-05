5c7cf6ff2e8b30717425dae669641a35b7ff2ccb
griselizr <griseliz.r.c15@gmail.com>
2022-07-05 15:13:05 +0000

    first commit

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "calc.h"
+
+/**
+ * op_add - sum
+ *@a: parameter
+ *@b: parameter
+ *Return: int
+ */
+int op_add(int a, int b);
+{
+	return (a + b);
+}
+/** op_sub - substraction
+ * @a: parameter
+ * @b: parameter
+ * Return: int
+ */
+int op_sub(int a, int b);
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication
+ * @a: parameter
+ * @b: parameter
+ *Return: int
+ */
+int op_mul(int a, int b);
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @a: parameter
+ * @b: parameter
+ * Return: int
+ */
+int op_div(int a, int b);
+{
+	return (a / b);
+}
+/**
+ * op_mod - modular
+ *@a: parameter
+ *@b: parameter
+ *Return: int
+ */
+int op_mod(int a, int b);
+{
+	return (a % b);
+}
