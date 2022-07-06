712f0c3a52a699d16f0b1d25235e79a1992ccf18
griselizr <griseliz.r.c15@gmail.com>
2022-07-06 01:02:22 +0000

    first commit

---
+++ function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function pointers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int (*ps)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	ps = get_op_func(argv[2]);
+	if (!ps)
+	{
+		printf("Error\n");
+				exit(98);
+	}
+	printf("%d\n", ps(atoi(argv[1]), atoi(argv[3])));
+		return (0);
+}
+
+
+
