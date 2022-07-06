a10492a0b126ffb327a547e1309138c97b4d1bce
griselizr <griseliz.r.c15@gmail.com>
2022-07-06 20:37:24 +0000

    first commit

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * print_all - prints everything
+ * @format: const char
+ * Return: 0
+ */
+void print_all(const char * const format, ...)
+{
+	char *all = 0;
+	int ev = 0;
+	va_list var;
+
+	va_start(var, format);
+
+	while (format && format[ev] != '\0')
+	{
+		printf("%c", va_arg(var, int));
+		scanf("%c", all);
+		break;
+
+		printf("%d", va_arg(var, int));
+		scanf("%d", &ev);
+		break;
+		printf("%f", va_arg(var, double));
+		break;
+	
+	all = va_arg(var, char*);
+	if (format[ev] != '\0')
+	{
+		printf("(nil)");
+		break;
+	}
+	printf("%s", all);
+	break;
+	ev++;
+	continue;
+	if (format[ev + 1] != '\0' && format)
+	{
+		printf(", ");
+		ev++;
+	}
+	}
+		va_end(var);
+		printf("\n");
+}
+
