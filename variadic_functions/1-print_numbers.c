aef8283effa77951554319dfef55b0d573c3522a
griselizr <griseliz.r.c15@gmail.com>
2022-07-06 17:29:10 +0000

    update

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -6,29 +6,23 @@
  * print_numbers - prints numbers
  * @n: number of integers
  * @separator: string
- * Return: numbers
+ * Return: 0
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int y = 0;
+	unsigned int y;
 	int uno;
 	va_list var;
 
 	va_start(var, n);
 
-	while (y < n)
+	for (y = 0; y < n; y++)
 	{
-		y++;
+		uno = va_arg(var, int);
+		printf("%d", uno);
 
-	uno = va_arg(var, int);
-	printf("%d", uno);
-
-	if (y < n - 1)
+	if (y < n - 1 && separator != NULL)
 		printf("%s", separator);
-
-	else if (separator != NULL)
-		printf("%s", separator);
-	y++;
 	}
 	printf("\n");
 	va_end(var);
