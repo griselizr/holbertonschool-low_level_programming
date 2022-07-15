c46e3480ae41eb2324ca4f57a129bae70636e850
griselizr <griseliz.r.c15@gmail.com>
2022-07-15 00:29:33 +0000

    first commit

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void__attributte__ ((constructor)) winner(void);
+
+/**
+* winner- prints a string
+* Return: none
+*/
+
+void winner(void)
+
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
