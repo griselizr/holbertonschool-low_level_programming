c203c9663caed769b3ee344b82d4c94547a2289d
griselizr <griseliz.r.c15@gmail.com>
2022-07-21 22:19:54 +0000

    betty style

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print a double linked list
+ * @h: header of double linked list
+ * Return: nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+		size_t c = 0;
+		int p = 0;
+
+		while (h != NULL)
+		{
+			c++;
+			if (h->prev != NULL && p == 0)
+			{
+				h = h->prev;
+				continue;
+			}
+		else if (h->prev == NULL)
+		{
+			p = 1;
+			c = 0;
+		}
+		printf("%i\n", h->n);
+		h = h->next;
+	}
+		return (c);
+}
