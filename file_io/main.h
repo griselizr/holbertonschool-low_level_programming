23bffe6769ae4e9fbb8b9f5db983826a5eec6f71
griselizr <griseliz.r.c15@gmail.com>
2022-07-23 01:40:13 +0000

    add prootypes

---
+++ file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef FILE_IO
+#define FILE_IO
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
