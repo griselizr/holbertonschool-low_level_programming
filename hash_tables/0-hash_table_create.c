b6b3b8f0d4f28848bd6b64a60be50781a6d837d0
griselizr <griseliz.r.c15@gmail.com>
2022-08-19 03:07:28 +0000

    fix typo description and betty style spacing

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,7 +2,7 @@
 
 /**
  *hash_table_create -unction that creates a hash table
- *@size - measurement of table
+ *@size:measurement of table
  *Return: hash table or null
  */
 
@@ -19,8 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 		return (NULL);
-	for (; box < size ;box++)
+	for ( ; box < size; box++)
 	table->array[box] = NULL;
-	
 	return (table);
 }
