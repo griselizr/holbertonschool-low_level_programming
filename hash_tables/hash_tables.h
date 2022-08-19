860784d5232b32a5c35200712302daed92ade6f9
griselizr <griseliz.r.c15@gmail.com>
2022-08-19 03:03:13 +0000

    add prototype

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,3 +1,6 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -29,4 +32,7 @@ typedef struct hash_table_s
      unsigned long int size;
      hash_node_t **array;
 } hash_table_t;
+
+
+hash_table_t *hash_table_create(unsigned long int size);
 #endif
