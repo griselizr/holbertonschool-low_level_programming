a280f4cddb074c060368c4fe1a31db209719a12c
griselizr <griseliz.r.c15@gmail.com>
2022-08-21 15:33:19 +0000

    add new fuction

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -35,27 +35,3 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	ht->array[i] = element;
 	return (1);
 }
-
-/**
- * connect_e - verify inside a bucket theres an element
- * @key: key
- * @array: bucket
- * @value: value of node
- * Return: 1 or 0
- */
-int connect_e(const char *key, hash_node_t *array, const char *value)
-{
-	hash_node_t *connect = array;
-
-	while (connect)
-	{
-		if (!(strcmp(connect->key, key)))
-		{
-			free(connect->value);
-			connect->value = strdup(value);
-			return (0);
-		}
-		connect = connect->next;
-	}
-	return (1);
-}
