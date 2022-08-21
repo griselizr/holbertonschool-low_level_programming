a33d29b7d56847cdb4653cd4e1a8eeabb0a7212d
griselizr <griseliz.r.c15@gmail.com>
2022-08-21 16:37:15 +0000

    add new file

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ * Return: hast table or null
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i;
+	unsigned int brace = 0;
+	hash_node_t *tmp = NULL;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			if (brace == 1)
+				printf(", ");
+				printf("'%s': '%s'", tmp->key, tmp->value);
+				brace = 1;
+				tmp = tmp->next;
+			}
+	}
+	printf("}\n");
+}
