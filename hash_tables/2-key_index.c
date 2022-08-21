178e76f3102c14e488aa4ffc7c078eddcbab884f
griselizr <griseliz.r.c15@gmail.com>
2022-08-21 14:29:29 +0000

    add new file

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+/**
+ * key_index - Write a function that gives you the index of a key
+ * @key: a key to use with has_djb2 function
+ * @size:s the size of the array of the hash table
+ * Return: 0
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
+
